32cb423835a7b8c8865247292c830d3e590d88da
Nils Balkow-Tychsen <nilsty@gmail.com>
2022-02-23 17:53:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Finley!\n'
+  const msg = 'Hello Nils!\n'
   res.end(msg);
 });
 
